a893bd3ffb5e33c6e30c71c375981bfbb51ace42
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-04-19 14:40:14 -0700

    main: ens name

--- components/login.js
+++ components/login.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react'
 import { ethers } from 'ethers'
 import Web3Modal from 'web3modal'
 import WalletConnectProvider from '@walletconnect/web3-provider'
+import { getName } from '../utils/name'
 
 const Login = () => {
   const [account, setAccount] = useState('')
+  const [name, setName] = useState(null)
   const [connection, setConnection] = useState(false)
   const [loggedIn, setLoggedIn] = useState(false)
 
@@ -45,6 +47,10 @@ const Login = () => {
     const signature = await signer.signMessage(user.nonce.toString())
     const response = await fetch(`/api/verify?address=${account}&signature=${signature}`)
     const data = await response.json()
+    const address = await signer.getAddress();
+    const ensName = await provider.lookupAddress(address);
+    const name = getName({ ensName, address });
+    setName(name)
     setLoggedIn(data.authenticated)
   }
 
@@ -59,7 +65,7 @@ const Login = () => {
           </div>
         )}
         {
-          loggedIn && <h3>Welcome, {account}</h3>
+          loggedIn && <h3>Welcome, {name || account}</h3>
          }
       </div>
   )
