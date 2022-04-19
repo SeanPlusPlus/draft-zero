158765f7447291e3f67b57cacba4bb771ab82e04
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-04-19 13:22:09 -0700

    main: remove vars

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { useContext } from 'react'
-import { GlobalContext } from '../context/GlobalState'
 import Head from 'next/head'
 
 // components
@@ -8,8 +6,6 @@ import Nav from '../components/nav'
 import Home from '../components/home'
 
 export default function App() {
-  const { user: { authenticated } } = useContext(GlobalContext)
-
   return (
     <>
       <Head>
