5c0a3c02ffe8cb2d8439bc4a012c1eb2556e0157
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-04-19 14:08:40 -0700

    main: add more db stuff

--- pages/api/verify.js
+++ pages/api/verify.js
@@ -5,6 +5,9 @@ import { users } from '../../utils/users'
 export default function verify(req, res) {
   let authenticated = false
   const {address, signature} = req.query
+
+  console.log('verify', users);
+
   const user = users[address]
   const decodedAddress = ethers.utils.verifyMessage(user.nonce.toString(), signature)
   if(address.toLowerCase() === decodedAddress.toLowerCase()) authenticated = true
