68841b1cc14a4a7a2ca9d68df95d1acc2f3874a6
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-04-19 14:08:44 -0700

    main: db

---
+++ db/collection/initialize.js
@@ -0,0 +1,29 @@
+const faunadb = require('faunadb')
+
+require('dotenv').config()
+
+const FAUNA_SECRET = process.env.FAUNA_SECRET
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: FAUNA_SECRET,
+  domain: "db.us.fauna.com",
+  port: 443,
+  scheme: 'https',
+})
+
+const {
+  CreateCollection,
+} = q
+
+
+const names = [
+  'users',
+]
+
+const history_days = null
+
+names.forEach((name) => {
+  client.query(
+    CreateCollection({name, history_days})
+  )
+})
