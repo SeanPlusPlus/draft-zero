734ea793981ec9ebfb8bfb4a98415fe6ad2a2ee7
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-04-21 13:14:02 -0700

    main: viewport

--- components/header.js
+++ components/header.js
@@ -5,6 +5,7 @@ const Header = () => {
     <Head>
       <title>Draft Zero</title>
       <meta name="description" content="Draft Zero" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   )
