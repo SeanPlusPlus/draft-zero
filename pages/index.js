ace9bc38034b9b4069c127864c2b2ca3796b8df1
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-04-21 11:22:11 -0700

    main: btn update

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,26 @@ const isProd = NODE_ENV === 'production';
 const METAMASK_URL = 'https://metamask.app.link/dapp/draft-zero.vercel.app/'
 
 export default function App() {
-  if (isMobile && isProd) {
+  if (isMobile && !isProd) {
     return (
-      <div className="min-h-screen">
-        <div className="text-center">
-          <Link href={METAMASK_URL}>
-            <button className="btn btn-outline px-10 mt-20">Open in MetaMask</button>
-          </Link>
+      <div className="min-h-screen grid-bg">
+        <Header />
+        <Nav />
+        <div className="hero">
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <Link href={METAMASK_URL}>
+                <button className="btn px-10 mt-20">
+                  <span className="mr-2">
+                    Open in MetaMask
+                  </span>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                  </svg>
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     )
