c9eb3dd2b5f9235f118aba91b69eb59ef61081e4
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-04-21 10:31:59 -0700

    main: redirect on mobile

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from 'react'
+import {isMobile} from 'react-device-detect'
+import Link from 'next/link'
 
 // components
 import Header from '../components/header'
 import Nav from '../components/nav'
 import Home from '../components/home'
 
+
 export default function App() {
+  if (isMobile) {
+    const METAMASK_URL = 'https://metamask.app.link/dapp/draft-zero.vercel.app/'
+    window.location.href = METAMASK_URL
+  }
+ 
   return (
     <div className="min-h-screen grid-bg">
       <Header />
