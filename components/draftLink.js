35634885e4a8bdaf9086c7d8e381853015162b1b
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-04-21 12:12:29 -0700

    main: only render page if options

--- components/draftLink.js
+++ components/draftLink.js
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import React, { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState'
 
+const year = '2022'
+
 const DraftLink = () => {
   const { imx } = useContext(GlobalContext)
 
@@ -13,8 +15,8 @@ const DraftLink = () => {
   
   return (
     <div className="mt-6">
-      <Link href="/nfl">
-        <a className="btn btn-secondary">Build your NFL 2022 Draft</a>
+      <Link href={`/nfl/${year}`}>
+        <a className="btn btn-secondary">Build your NFL {year} Draft</a>
       </Link>
     </div>
   )
--- pages/nfl/[year].js
+++ pages/nfl/[year].js
@@ -57,30 +57,32 @@ export default function NFL() {
       <Header />
       <Nav />
       <div className="hero">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">NFL 2022</h1>
-            <p className="py-6">
-              Predict the order for the 2022 NFL Draft
-            </p>
-            <div className="card md:w-96 bg-base-100 shadow-xl">
-              <div className="card-body">
-                <ul>
-                  {picks.map((p, i) => (
-                    <li key={i}>
-                      <select className="select select-bordered  w-full max-w-xs mb-3" onChange={handleChange}>
-                        <option>#{i + 1}</option>
-                        {options && options.map((o, idx) => (
-                          <option key={idx} value={`${i}:${o}`}>{o}</option>
-                        ))}
-                      </select>
-                    </li>
-                  ))}
-                </ul>
+        {options.length > 0 && (
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <h1 className="text-5xl font-bold">NFL {year}</h1>
+              <p className="py-6">
+                Predict the order for the {year} NFL Draft
+              </p>
+              <div className="card md:w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                  <ul>
+                    {picks.map((p, i) => (
+                      <li key={i}>
+                        <select className="select select-bordered  w-full max-w-xs mb-3" onChange={handleChange}>
+                          <option>#{i + 1}</option>
+                          {options.map((o, idx) => (
+                            <option key={idx} value={`${i}:${o}`}>{o}</option>
+                          ))}
+                        </select>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   )
