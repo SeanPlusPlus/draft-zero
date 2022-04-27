e447bb8fbe195d7e50c90d8316c3b9343a87efb2
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-04-26 20:03:16 -0700

    main: flex fix

--- pages/leaderboard/[draft_name].js
+++ pages/leaderboard/[draft_name].js
@@ -126,8 +126,10 @@ export default function Leaderboard() {
                                 <tr key={i}>
                                   <th>{i + 1}</th>
                                   <td>
-                                    <div className="link link-info flex" id={entry.id} onClick={handleModal}>
-                                      {entry.name}
+                                    <div className="flex">
+                                      <div className="link link-info" id={entry.id} onClick={handleModal}>
+                                        {entry.name}
+                                      </div>
                                       {entry.official && (
                                         <span className="ml-2 tooltip" data-tip="Official ESPN Expert">
                                           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
