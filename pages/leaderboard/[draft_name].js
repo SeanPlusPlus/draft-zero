65452ce60669872dfade645bf5aaf766be2f902e
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-04-26 19:59:34 -0700

    main: more fixes

--- pages/leaderboard/[draft_name].js
+++ pages/leaderboard/[draft_name].js
@@ -126,8 +126,15 @@ export default function Leaderboard() {
                                 <tr key={i}>
                                   <th>{i + 1}</th>
                                   <td>
-                                    <div className="link link-info" id={entry.id} onClick={handleModal}>
+                                    <div className="link link-info flex" id={entry.id} onClick={handleModal}>
                                       {entry.name}
+                                      {entry.official && (
+                                        <span className="ml-2 tooltip" data-tip="Official ESPN Expert">
+                                          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
+                                          </svg>
+                                        </span>
+                                      )}
                                     </div>
                                   </td>
                                   <td>{getScores(entry, index)}</td>
