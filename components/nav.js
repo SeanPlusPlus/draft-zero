0af26d7a2ebcfc893d0430545a1b91daba94853f
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-04-27 15:01:52 -0700

    main: close icon on about

--- components/nav.js
+++ components/nav.js
@@ -134,7 +134,8 @@ const Nav = () => {
       </div>
 
       <div className={`modal ${modalAbout}`}>
-        <div className="modal-box">
+        <div className="modal-box relative">
+          <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2" onClick={handleCloseAbout}>✕</label>
           <h3 className="font-bold text-xl flex">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
