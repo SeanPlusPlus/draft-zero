ec3d1e7d272e4a9968b0a2b95d8112653aad0012
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-04-26 21:08:24 -0700

    main: header update

--- components/header.js
+++ components/header.js
@@ -7,6 +7,19 @@ const Header = () => {
       <meta name="description" content="Draft Zero" />
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       <link rel="icon" href="/favicon.ico" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://draft-zero.vercel.app/" />
+      <meta property="og:title" content="Draft Zero" />
+      <meta property="og:description" content="Build a prediction for any draft" />
+      <meta property="og:image" content="https://i.imgur.com/Jg3rU2i.png" />
+      
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:domain" value="draft-zero.vercel.app" />
+      <meta name="twitter:title" value="Draft Zero" />
+      <meta name="twitter:description" value="Build a prediction for any draft" />
+      <meta name="twitter:image" content="https://i.imgur.com/Jg3rU2i.png" />
+      <meta name="twitter:url" value="https://draft-zero.vercel.app/" />
     </Head>
   )
 }
