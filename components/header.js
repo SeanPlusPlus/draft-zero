70aaf36f26b5b35563136412f921f33a6d834b3b
Sean Stephenson <sean.m.stephenson@disney.com>
2023-04-26 13:59:49 -0700

    main: logo update

--- components/header.js
+++ components/header.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 
+const logo = 'https://cdn.midjourney.com/d55ce8a3-950b-46c1-9cb9-351ca8ad8007/0_1.png'
+
 const Header = () => {
   return (
     <Head>
@@ -12,13 +14,13 @@ const Header = () => {
       <meta property="og:url" content="https://draft-zero.vercel.app/" />
       <meta property="og:title" content="Draft Zero" />
       <meta property="og:description" content="Build a prediction for any draft" />
-      <meta property="og:image" content="https://i.imgur.com/Jg3rU2i.png" />
+      <meta property="og:image" content={logo} />
       
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:domain" value="draft-zero.vercel.app" />
       <meta name="twitter:title" value="Draft Zero" />
       <meta name="twitter:description" value="Build a prediction for any draft" />
-      <meta name="twitter:image" content="https://i.imgur.com/Jg3rU2i.png" />
+      <meta name="twitter:image" content={logo} />
       <meta name="twitter:url" value="https://draft-zero.vercel.app/" />
     </Head>
   )
