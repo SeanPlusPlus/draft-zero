d3aa26f6c2c265fc62e306ae82772c9731da0fdc
Sean Stephenson <sean.m.stephenson@disney.com>
2023-04-26 15:00:05 -0700

    main: mobile tweaks

--- components/draftLink.js
+++ components/draftLink.js
@@ -16,16 +16,16 @@ const DraftLink = () => {
   return (
     <div className="mt-6">
       <div className="card md:w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
+        <div className="card-body pl-3 pr-3 md:pl-8 md:pr-8">
           <h2 className="card-title">{featured.description}</h2>
           <div className="btn-group">
             {!featured.closed && (
               <Link href={featured.draft_link}>
-                <a className="btn btn-secondary md:w-40">Make your picks</a>
+                <a className="btn btn-secondary md:w-40 text-xs">Make your picks</a>
               </Link>
             )}
             <Link href={featured.leaderboard_link}>
-              <a className="btn btn-outline md:w-40">Live Leaderboard</a>
+              <a className="btn btn-outline md:w-40 text-xs">Live Leaderboard</a>
             </Link>
           </div>
         </div>
